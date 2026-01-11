f47266bf09ec74a44a525a90e8c9f537c50e9d06
Enkhnasan03 <enkhush.c@gmail.com>
2026-01-11 17:11:55 +0800

    fix

--- src/components/BabyBirthdayInvite.jsx
+++ src/components/BabyBirthdayInvite.jsx
@@ -125,13 +125,13 @@ export default function BabyBirthdayInvite() {
 
   <Star className="absolute -top-4 -right-4 w-6 h-6 text-yellow-400 animate-twinkle" />
   <Gift className="absolute -bottom-4 -left-4 w-6 h-6 text-purple-400 animate-float-alt" />
-  <Rocket className="absolute -top-6 left-1/2 w-5 h-5 text-orange-400 animate-spin-slow" />
-  <Moon className="absolute -bottom-4 -right-6 w-6 h-6 text-cyan-300 animate-spin-slow" />
+  <Rocket className="absolute -top-8 left-1/2 w-5 h-5 text-orange-400 animate-spin-slow" />
+  <Moon className="absolute -bottom-4 -right-8 w-6 h-6 text-cyan-300 animate-spin-slow" />
    <Star className="absolute -bottom-8 -left-16 w-6 h-6 text-red-400 animate-twinkle" />
   <Gift className="absolute -top-8 -right-16 w-6 h-6 text-green-300 animate-float-alt" />
   <Rocket className="absolute -bottom-9 left-20 w-5 h-5 text-blue-500 animate-spin-slow" />
   <Moon className="absolute -bottom-2 -left-20 w-6 h-6 text-orange-300 animate-spin-slow" />
-  <Satellite className="absolute -top-8 -left-16 w-6 h-6 text-gray-400 animate-float-alt" />
+  <Satellite className="absolute -top-8 -left-16 w-6 h-6 text-gray-400 animate-spin-slow" />
 </div>
 
               <div className="absolute bottom-0 w-full h-1/2 bg-linear-to-t from-white to-transparent" />
